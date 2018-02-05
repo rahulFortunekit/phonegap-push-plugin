91136c51f8e8e5cf770a12efb6e787541e406c3c
Rahul M <rahul@fourzip.com>
2018-02-05 16:28:07 +0530

    modified version according to applozic

---
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -0,0 +1,40 @@
+package com.adobe.phonegap.push;
+
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import com.applozic.mobicomkit.Applozic;
+import com.applozic.mobicomkit.api.account.register.RegisterUserClientService;
+import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
+import com.applozic.mobicomkit.api.account.user.MobiComUserPreference;
+
+public class PushInstanceIDListenerService extends FirebaseInstanceIdService implements PushConstants {
+    public static final String LOG_TAG = "Push_InsIdService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Applozic.getInstance(this).setDeviceRegistrationId(refreshedToken);
+        if (MobiComUserPreference.getInstance(this).isRegistered()) {
+            try {
+                RegistrationResponse registrationResponse = new RegisterUserClientService(this).updatePushNotificationId(refreshedToken);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
